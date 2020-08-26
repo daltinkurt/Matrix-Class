f67e9ba5084a01c581962a1f42d9658345b99430
daltinkurt <daltinkurt@hotmail.com>
2020-08-27 00:52:29 +0300

    Add project files.

---
+++ Matrix/Matrix.cs
@@ -0,0 +1,107 @@
+﻿using System;
+
+namespace DA
+{
+    public class Matrix<T>
+    {
+        private int n; // boyut sayısı
+        private int[] d; // her bir boyuttaki eleman sayısı (d[0], d[1], ... , d[n-1])
+        private T[] dizi; // tutulacak değerler
+        private int max; // toplam eleman sayısı = d[0] x d[1] x d[2] x ... x d[n-1]
+
+        public Matrix(params int[] a) // boyutlar
+        {
+            Resize(a);
+        }
+
+        public Matrix()
+        {
+            Clear();
+        }
+
+        public void Clear()
+        {
+            n = -1;
+            d = null;
+            dizi = null;
+            max = 0;
+        }
+
+        private void Resize(params int[] a)
+        {
+            if (a.Length == 0) throw new Exception("Boyut hatası");
+            n = a.Length;
+            d = new int[n];
+            max = 1;
+            for (int i = 0; i < n; i++)
+            {
+                if (a[i] < 0) throw new Exception("Boyut hatası");
+                d[i] = a[i];
+                max *= d[i];
+            }
+            dizi = new T[max];
+        }
+
+        public T GetValue(params int[] a)
+        {
+            return dizi[Pos(a)];
+        }
+
+        public void SetValue(T t, params int[] a)
+        {
+            dizi[Pos(a)] = t;
+        }
+
+        private int Pos(params int[] a)
+        {
+            if (n < 0) throw new Exception("Matrix hazır değil");
+            if (a.Length != d.Length) throw new Exception("Boyut hatası");
+            int pos = 0;
+            for (int i = 0; i < n; i++)
+            {
+                if (a[i] >= d[i]) throw new Exception("Boyut hatası");
+                int x = 1;
+                for (int j = i + 1; j < n; j++)
+                    x *= d[j];
+                pos += a[i] * x;
+            }
+            return pos;
+        }
+
+        public T this[int a]
+        {
+            get { return dizi[a]; }
+            set { dizi[a] = value; }
+        }
+
+        public T this[params int[] a]
+        {
+            get { return GetValue(a); }
+            set { SetValue(value, a); }
+        }
+
+        public int Length
+        {
+            get
+            {
+                return max;
+            }
+        }
+
+        public int GetDimension
+        {
+            get
+            {
+                if (n < 0) throw new Exception("Boyut hatası");
+                return n;
+            }
+        }
+
+        public int GetLength(int x)
+        {
+            if (n < 0 || n <= x) throw new Exception("Boyut hatası");
+            if (n < 0) throw new Exception("Boyut hatası");
+            return d[x];
+        }
+    }
+}
---
+++ Matrix/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Matrix<int> dizi = new Matrix<int>(4, 3, 2);
+
+            // değer atama
+            dizi[2, 2, 1] = 500;
+            dizi.SetValue(500, 2, 2, 1);
+
+            // değer okuma
+            var x = dizi[2, 2, 1];
+            var x2 = dizi.GetValue(2, 2, 1);
+
+            // sıfırlama
+            //dizi.Clear();
+
+            // elemanların pozisyonunu kullanma
+            dizi[17] = 500;
+            // aynısı
+            dizi[2, 2, 1] = 400;
+
+
+            // eleman sayısı
+            var adet = dizi.Length;
+
+            // Boyut sayısı
+            var dim = dizi.GetDimension;
+
+            // X. boyyuttaki eleman sayısı
+            var adetX = dizi.GetLength(1);
+
+            // dizinin tüm elemanlarını yazdırma
+            for (int i = 0; i < dizi.Length; i++)
+            {
+                Console.Write(dizi[i] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
